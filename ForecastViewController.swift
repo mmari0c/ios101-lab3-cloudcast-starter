ad058e12d5432845c79904687726824e9c2d31c5
Mario Nolasco Cortez <mariocortez7745@gmail.com>
2025-10-07 21:20:13 -0500

    Finished fr now

--- ForecastViewController.swift
+++ ForecastViewController.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class ForecastViewController: UIViewController {
-
+    
+    private var forecasts = [WeatherForecast]()
+    private var selectedForecastIndex = 0
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var forecastImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        selectedForecastIndex = max(0, selectedForecastIndex - 1)
+        configure(with: forecasts[selectedForecastIndex])
+    }
+    @IBAction func didTapForwardButton(_ sender: Any) {
+        selectedForecastIndex = min(forecasts.count - 1, selectedForecastIndex + 1)
+        configure(with: forecasts[selectedForecastIndex])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        forecasts = createMockData()
+        configure(with: forecasts[selectedForecastIndex])
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createMockData() -> [WeatherForecast] {
+        let today = Date()
+        var dateComponents = DateComponents()
+        dateComponents.day = 1
+        let tomorrow = Calendar.current.date(byAdding: dateComponents, to: today)!
+        let dayAfterTomorrow = Calendar.current.date(byAdding: dateComponents, to: tomorrow)!
+        
+        let mockData1 = WeatherForecast(windSpeed: 3.3, weatherCode: .violentRainShowers, precipitation: 33.3, temperature: 59.5, date: today)
+        
+        let mockData2 = WeatherForecast(windSpeed: 3.7, weatherCode: .fog, precipitation: 13.3, temperature: 65.5, date: tomorrow)
+        
+        let mockData3 = WeatherForecast(windSpeed: 2.8, weatherCode: .partlyCloudy, precipitation: 13.3, temperature: 49.5, date: dayAfterTomorrow)
+        
+        return [mockData1, mockData2, mockData3]
     }
-    */
-
+    
+    private func configure(with forecast: WeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)°F"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: forecast.date)
+    }
 }
--- WeatherForecast.swift
+++ WeatherForecast.swift
@@ -5,4 +5,72 @@
 //  Created by Mario Cortez on 10/7/25.
 //
 
+import UIKit
+
+struct WeatherForecast {
+    let windSpeed: Double
+    let weatherCode: WeatherCode
+    let precipitation: Double
+    let temperature: Double
+    let date: Date
+}
+
+enum WeatherCode {
+    case clearSky
+    case mainlyClear
+    case partlyCloudy
+    case overcast
+    case fog
+    case lightDrizzle
+    case moderateDrizzle
+    case denseDrizzle
+    case slightRainShowers
+    case moderateRainShowers
+    case violentRainShowers
+    
+    var description: String {
+        switch self {
+            case .clearSky: 
+                return "Clear Sky"
+            case .mainlyClear: 
+                return "Mainly Clear"
+            case .partlyCloudy:
+                return "Partly Cloudy"
+            case .overcast:
+                return "Overcast"
+            case .fog:
+                return "Fog"
+            case .lightDrizzle:
+                return "Light Drizzle"
+            case .moderateDrizzle:
+                return "Moderate Drizzle"
+            case .denseDrizzle:
+                return "Dense Drizzle"
+                case .slightRainShowers:
+                return "Slight Rain Showers"
+            case .moderateRainShowers:
+                return "Moderate Rain Showers"
+            case .violentRainShowers:
+                return "Violent Rain Showers"
+        }
+    }
+    
+    var image: UIImage? {
+        switch self {
+            case .clearSky, .mainlyClear:
+                return UIImage(named: "sun")
+            case .partlyCloudy:
+                return UIImage(named: "cloud-sun")
+            case .overcast:
+                return UIImage(named: "cloud")
+            case .fog:
+                return UIImage(named: "fog")
+            case .lightDrizzle, .moderateDrizzle, .denseDrizzle:
+                return UIImage(named: "drizzle")
+            case .slightRainShowers, .moderateRainShowers, .violentRainShowers:
+                return UIImage(named: "cloud-drizzle")
+        }
+    }
+}
+
 import Foundation
